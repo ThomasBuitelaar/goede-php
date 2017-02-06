78d6193248a0cad323dcbb7d43b4e2ff4bf7ef8f
Thomas Buitelaar <99041950@mydavinci.nl>
2017-02-06 14:51:17 +0100

    Update Index

--- index.php
+++ index.php
@@ -2,14 +2,19 @@
 <html>
 <head>
 <link rel="stylesheet" href="style/stylesheet.css">
-<title></title>
+<title>What time is it?</title>
 </head>
 <body>
 <script type="text/javascript" src="javaScript.js"></script>
-
+<div id="">
 	<?php 
+
+		date_default_timezone_set('')
+		$time = date("H:i:s");
 
 
 	?>
+
+</div>
 </body>
 </html>
