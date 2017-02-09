b45a5e64cd5f2787626e9ac092f51c8d4610d55f
Thomas Buitelaar <99041950@mydavinci.nl>
2017-02-09 10:12:09 +0100

    Update index: Added time

--- index.php
+++ index.php
@@ -2,19 +2,23 @@
 <html>
 <head>
 <link rel="stylesheet" href="style/stylesheet.css">
-<title>What time is it?</title>
+<title></title>
 </head>
 <body>
-<script type="text/javascript" src="javaScript.js"></script>
-<div id="">
+<script type="text/javascript" src="javaScript.js">
+
+		</script>
+<div id=reload>
 	<?php 
-
-		date_default_timezone_set('')
-		$time = date("H:i:s");
-
+		date_default_timezone_set('Europe/Amsterdam');
+		$time = date("H:i");
+		$text = "";
+		if($time >= "06:00" and $time < "12:00"){
+			$text = "Good morning!";
+			echo "<h1>$text</h1><p>The time is $time</p>";
+		}
 
 	?>
-
-</div>
+	</div>
 </body>
 </html>
