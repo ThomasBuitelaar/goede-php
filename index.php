0183deb3df6f1dda630edb7cb38f1b1ebd9df00f
Thomas Buitelaar <99041950@mydavinci.nl>
2017-02-09 10:28:48 +0100

    update index: added id on body added background on morning

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="style/stylesheet.css">
 <title></title>
 </head>
-<body>
+<body id =  "background">
 <script type="text/javascript" src="javaScript.js">
 
 		</script>
@@ -16,12 +16,16 @@
 		if($time >= "06:00" and $time < "12:00"){
 			$text = "Good morning!";
 			echo "<h1>$text</h1><p>The time is $time</p>";
+			echo "<style>#background{background-image:url(backgrounds/morning.png); background-attachment: fixed;  background-size: cover;}</style>";
 		} elseif ($time >= "12:00" and $time < "18:00") {
 			$text = "Good afternoon!";
 			echo "<h1>$text</h1><p>The time is $time</p>";
 		} elseif ($time >= "18:00" and $time < "24:00") {
 			$text = "Good evening!";
 			echo "<h1>$text</h1><p>The time is $time</p>";
+		} elseif ($time >= "00:00" and $time < "06:00") {
+			$text = "Good night!"
+			echo "<h1>$text</h1><p>The time is $time</p>"
 		}
 		
 
