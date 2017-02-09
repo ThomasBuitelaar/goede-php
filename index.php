6a3b78525785439697e5cc147e980529b3159885
Thomas Buitelaar <99041950@mydavinci.nl>
2017-02-09 12:39:06 +0100

    fixed backgrounds

--- index.php
+++ index.php
@@ -27,14 +27,15 @@ var timeout = setTimeout(reload, 1000);
 		} elseif ($time >= "12:00" and $time < "18:00") {
 			$text = "Good afternoon!";
 			echo "<h1>$text</h1><p>The time is $time</p>";
-			echo "<style>#background{background-image url(backgrounds/afternoon.png); background-attachment: fixed; background-size: cover;}</style>";
+			echo "<style>#background{background-image:url(backgrounds/afternoon.png); background-attachment: fixed; background-size: cover;}</style>";
 		} elseif ($time >= "18:00" and $time < "24:00") {
 			$text = "Good evening!";
-			echo "<h1>$text</h1><p>The time is $time</p>";echo "<style>#background{background-image url(backgrounds/evening.png); background-attachment: fixed; background-size: cover;}</style>";
+			echo "<h1>$text</h1><p>The time is $time</p>";
+			echo "<style>#background{background-image:url(backgrounds/evening.png); background-attachment: fixed; background-size: cover;}</style>";
 		} elseif ($time >= "00:00" and $time < "06:00") {
 			$text = "Good night!";
 			echo "<h1>$text</h1><p>The time is $time</p>";
-			echo "<style>#background{background-image url(backgrounds/night.png); background-attachment: fixed; background-size: cover;}</style>";
+			echo "<style>#background{background-image:url(backgrounds/night.png); background-attachment: fixed; background-size: cover;}</style>";
 		}
 
 	?>
